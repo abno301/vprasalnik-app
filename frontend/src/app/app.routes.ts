134f28c5bebaa08eda2ca71843be47ad17bd9a9b
abno301 <jernej.hozjan12@gmail.com>
2024-04-04 11:16:03 +0200

    vprasalnik component initialized, routing set, vprasanja.json added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {VprasalnikComponent} from "./components/uporabnik/vprasalnik/vprasalnik.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path:'', redirectTo:'index.html',pathMatch:"full" },
+  { path:"index.html", component:VprasalnikComponent },
+];
---
+++ frontend/src/app/components/uporabnik/vprasalnik/vprasalnik.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vprasalnik',
+  standalone: true,
+  imports: [],
+  templateUrl: './vprasalnik.component.html',
+  styleUrl: './vprasalnik.component.css'
+})
+export class VprasalnikComponent {
+
+}
